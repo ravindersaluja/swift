9bbf2f5c1d649712da8b3f7cfc042a2448b17d93
Ravinder Saluja <ravz.saluja@yahoo.com>
2020-04-17 01:46:08 +0530

    String Interpolation in Swift

---
+++ Basics.playground/Contents.swift
@@ -0,0 +1,14 @@
+//Initializing Variables
+var foo = 5
+print(foo)
+var boo = 10
+print(boo)
+
+//String Interpolation in Swift
+
+print("Value of foo is: \(foo)")
+//Python like also works
+print("Value of foo is:", foo)
+//or
+print("Value of foo is: "+String(foo))
+
